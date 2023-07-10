b73c2e26fd238db8cf200c590a8c0714ef0bb766
quanvu45 <nadarinojugen123@gmail.com>
2023-07-10 17:37:41 +0700

    fix the chosewordfromlist

--- AdvProg_L3-HangMan/hangman.cpp
+++ AdvProg_L3-HangMan/hangman.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include "hangman.h"
-
+#include<bits/stdc++.h>
 using std::string;
 using std::vector;
 using std::ifstream;
@@ -72,6 +72,7 @@ string chooseWordFromList(const vector<string>& wordList, int index)
     // TODO: Return a lowercase word in the index position of the vector wordList.
     string answer;
     answer=wordList[index];
+    transform(answer.begin(),answer.end(),answer.begin(),::tolower);
     return answer;
 }
 
