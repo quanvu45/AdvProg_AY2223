8364b8cfa726d626c335ad3f94a47f3f1d32afcc
quanvu45 <nadarinojugen123@gmail.com>
2023-08-09 11:40:47 +0700

    update

--- AdvProg_L7-Snake/Snake.cpp
+++ AdvProg_L7-Snake/Snake.cpp
@@ -26,6 +26,11 @@ Snake::~Snake()
             p = nextNode;
     }
     */
+   for(SnakeNode* p=tail;p!=nullptr;){
+    SnakeNode *next=p->next;
+    delete p;
+    p=next;
+   }
 }
 
 // DO NOT CHANGE METHOD
@@ -53,7 +58,9 @@ vector<Position> Snake::getPositions() const
 void Snake::growAtFront(Position newPosition)
 {
     // head of snake grow at new position
-	
+	SnakeNode* newHead=new SnakeNode(newPosition);
+    head->next=newHead;
+    head=head->next;
     /* YOUR CODE HERE */
 }
 
@@ -85,9 +92,14 @@ void Snake::slideTo(Position newPosition)
 	if (tail->next == nullptr) { 
         // position is assigned by new position.
 		/* YOUR CODE HERE */
+        tail->position=newPosition;
 	}
 	else {
 		SnakeNode *oldTailNode = tail;
+        tail=tail->next;
+        oldTailNode->next=nullptr;
+        oldTailNode position =newPosition;
+        head->next=oldTailNode;
 		//cut the old tail off the snake
         /* YOUR CODE HERE */
 		
@@ -111,7 +123,7 @@ void Snake::slideTo(Position newPosition)
 ***/
 void Snake::eatCherry()
 {
-	/* YOUR CODE HERE */
+	/* YOUR CODE HERE */cherry++;score++;
 }
 
 /*** 
@@ -143,17 +155,22 @@ void Snake::eatCherry()
 void Snake::move(Direction direction)
 {
     Position newPosition = head->position.move(direction);
+    game.snakeMoveTo(newPosition);
 
     /* YOUR CODE HERE */
-    
+    if(game.isGameOver()){
+        return;
+    }
     // If gameOver, return ; 
     /* YOUR CODE HERE */
-
     // If cherry > 0, cherry descrease one and growAtFront() with newPosition
     if (cherry > 0) {
         /* YOUR CODE HERE */
+        cherry--;
+        growAtFront(newPosition);
     } else {
     	game.snakeLeave(tail->position);
+        slideTo(newPosition);
         /* YOUR CODE HERE */        
     }
 }
